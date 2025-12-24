9414364522b596727ae7c36e1e15ec3a4aa094e6
Shraddha0119 <gunjkarshraddha6@gmail.com>
2025-12-24 18:40:23 +0530

    server creating

--- express/server_2/index.js
+++ express/server_2/index.js
@@ -1,8 +1,8 @@
-// import express from "express"
-// import fs from  "fs"
+import express from "express"
+import fs from  "fs"
 
-// const app = express()
-// let port = 4000
+const app = express()
+let port = 4000
 
 
 
@@ -12,12 +12,12 @@
 
 
 
-// app.get("/getdata",(req,res)=>{
-//     fs.readFile("data.json","utf-8",(err,data)=>{
-//         if(err) res.send(err)
-//         else    res.send(JSON.parse(data))
-//     })
-// })
+app.get("/getdata",(req,res)=>{
+    fs.readFile("data.json","utf-8",(err,data)=>{
+        if(err) res.send(err)
+        else    res.send(JSON.parse(data))
+    })
+})
 
 
 // app.post("/postdata",(req,res)=>{
@@ -42,14 +42,6 @@
 
 
 
-
-
-
-
-
-
-
-
 // // app.get('/',(req,res)=>{
 // // res.send("i am coming")
 // // })
@@ -58,44 +50,44 @@
 // // res.send("i am datamaster")
 // // })
 
-// app.listen(port,()=>{
-//     console.log(`server is running http://localhost:${port}`);
+app.listen(port,()=>{
+    console.log(`server is running http://localhost:${port}`);
     
-// })
+})
 
-import express from "express";
-import fs from "fs";
+// import express from "express";
+// import fs from "fs";
 
-const app = express();
-let port = 4000;
+// const app = express();
+// let port = 4000;
 
-app.use(express.json());
+// app.use(express.json());
 
 // GET DATA
-app.get("/getdata", (req, res) => {
-    fs.readFile("data.json", "utf-8", (err, data) => {
-        if (err) return res.send(err);
+// app.get("/getdata", (req, res) => {
+//     fs.readFile("data.json", "utf-8", (err, data) => {
+//         if (err) return res.send(err);
 
-        res.send(JSON.parse(data));
-    });
-});
+//         res.send(JSON.parse(data));
+//     });
+// });
 
 // POST DATA
-app.post("/postdata", (req, res) => {
-    fs.readFile("data.json", "utf-8", (err, data) => {
-        if (err) return res.send(err);
+// app.post("/postdata", (req, res) => {
+//     fs.readFile("data.json", "utf-8", (err, data) => {
+//         if (err) return res.send(err);
 
-        let jasondata = JSON.parse(data);
+//         let jasondata = JSON.parse(data);
 
-        jasondata.users.push(req.body);
+//         jasondata.users.push(req.body);
 
-        fs.writeFile("data.json", JSON.stringify(jasondata,  2), (err) => {
-            if (err) return res.send(err);
+//         fs.writeFile("data.json", JSON.stringify(jasondata,  2), (err) => {
+//             if (err) return res.send(err);
 
-            res.send("data post done");
-        });
-    });
-});
+//             res.send("data post done");
+//         });
+//     });
+// });
 
 app.listen(port, () => {
     console.log(`server is running http://localhost:${port}`);
