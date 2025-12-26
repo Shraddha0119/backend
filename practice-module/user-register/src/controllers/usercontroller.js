0ef9b2b35b840f13632960fd7fdc14a84d27ca1b
Shraddha0119 <gunjkarshraddha6@gmail.com>
2025-12-26 13:18:42 +0530

    user-register

--- practice-module/user-register/src/controllers/usercontroller.js
+++ practice-module/user-register/src/controllers/usercontroller.js
@@ -1,4 +1,4 @@
-import User from "../models/User.js";
+import user from "../model/usermodel.js";
 
 export const registerUser = async (req, res) => {
   const user = await User.create(req.body);
--- practice-module/user-register/src/server.js
+++ practice-module/user-register/src/server.js
@@ -1,9 +1,22 @@
 import express from "express";
+import dotenv from "dotenv";
 import userRoutes from "./routes/userRoutes.js";
+
+dotenv.config();
 
 const app = express();
 
 app.use(express.json());
+
+// ✅ ROOT ROUTE (FOR BROWSER)
+app.get("/", (req, res) => {
+  res.send("🚀 Server is running successfully!");
+});
+
 app.use("/api", userRoutes);
 
-app.listen(4000, () => console.log("Server is  running"));
+const PORT = process.env.PORT || 8085;
+
+app.listen(PORT, () => {
+  console.log(`server is running at http://localhost:${PORT}`);
+});
