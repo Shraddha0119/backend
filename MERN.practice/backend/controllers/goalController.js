e5de5ee00e3482f973941c8ad81c678cd73fe130
Shraddha0119 <gunjkarshraddha6@gmail.com>
2026-01-06 15:57:38 +0530

    backend practice

--- MERN.practice/backend/controllers/goalController.js
+++ MERN.practice/backend/controllers/goalController.js
@@ -1,9 +1,42 @@
-
-
+// @desc Get goal
+// @route Get/api/goals
+// @access Private
 const getGoals = (req,res) =>{
         res.status(200).json({ message: 'get goals'})
 }
 
-module.exports = {getGoals,}
+// @desc Get goal
+// @route post/api/goals
+// @access Private
+const setGoal = (req, res) => {
+       if (!req.body.text){
+         res.status(400).json({ message:'Plaease add a text field'})
+        }
+        
+        res.status(200).json({ message: 'Set goals'})
+}
+
+// @desc update goal
+// @route put/api/goals/:id
+// @access Private
+const updateGoal = (req,res) =>{
+       res.status(200).json({ message: `update goals ${req.params.id}`})
+}
+
+// @desc Delete goal
+// @route delete/api/goals/:id
+// @access Private
+const deleteGoal = (req,res) =>{
+   res.status(200).json({ message: `delete goals ${req.params.id}`})
+}
+
+
+module.exports = {
+ getGoals,
+ setGoal,
+ updateGoal,
+ deleteGoal,
+
+}
 
 // 22min
--- MERN.practice/backend/routes/goalRoutes.js
+++ MERN.practice/backend/routes/goalRoutes.js
@@ -1,19 +1,12 @@
 const express = require('express')
 const router = express.Router()
-const {getGoals} = require('../controllers/goalController')
+const {getGoals,setGoal,updateGoal,deleteGoal} = require('../controllers/goalController')
 
-router.get('/',getGoals)
+router.route('/').get(getGoals).post(setGoal)
+router.route('/:id').put(updateGoal).delete(deleteGoal)
 
-router.post('/',(req,res)=>{
-    res.status(200).json({ message: 'Set goals'})
-})
 
-router.put('/:id',(req,res)=>{
-    res.status(200).json({ message: `update goals ${req.params.id}`})
-})
 
-router.delete('/:id',(req,res)=>{
-   res.status(200).json({ message: `delete goals ${req.params.id}`})
-})
+
 
 module.exports = router
